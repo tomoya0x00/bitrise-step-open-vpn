e75e5b17fea4f175f1294e2faf60a9fce6ea636e
tomoya.miwa <tomoyann@gmail.com>
2018-02-02 20:07:35 +0900

    fix exception of ensure_git_status_clean

--- step.sh
+++ step.sh
@@ -39,10 +39,11 @@ EOF
   darwin*)
     echo "Configuring for Mac OS"
 
-    echo ${ca_crt} | base64 -D -o ca.crt > /dev/null 2>&1
-    echo ${client_crt} | base64 -D -o client.crt > /dev/null 2>&1
-    echo ${client_key} | base64 -D -o client.key > /dev/null 2>&1
+    echo ${ca_crt} | base64 -D -o /var/tmp/ca.crt > /dev/null 2>&1
+    echo ${client_crt} | base64 -D -o /var/tmp/client.crt > /dev/null 2>&1
+    echo ${client_key} | base64 -D -o /var/tmp/client.key > /dev/null 2>&1
 
+    cd /var/tmp/
     sudo openvpn --client --dev tun --proto udp --remote ${host} 1194 --resolv-retry infinite --nobind --persist-key --persist-tun --comp-lzo --verb 3 --ca ca.crt --cert client.crt --key client.key > /dev/null 2>&1 &
 
     sleep 5
